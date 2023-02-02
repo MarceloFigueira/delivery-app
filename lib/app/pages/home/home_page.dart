4868a37dd86518ba16408657034f51d581ab7470
Marcelo Figueira <marcelofigueira87@gmail.com>
2023-02-01 21:50:30 -0300

    tela de produto parcialmente montada

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:delivery_app/app/core/widgets/delivery_appbar.dart';
+import 'package:delivery_app/app/models/product_model.dart';
+import 'package:delivery_app/app/pages/home/widgets/delivery_product_tile.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,7 +10,26 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: DeliveryAppbar(),
-      body: Container(),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return DeliveryProductTile(
+                    product: ProductModel(
+                        id: 0,
+                        name: 'X-Burger',
+                        description:
+                            'Lanche acompanha pão, hambúguer, mussarela e maionese',
+                        price: 15.0,
+                        image:
+                            'https://assets.unileversolutions.com/recipes-v2/106684.jpg?imwidth=800'),
+                  );
+                }),
+          ),
+        ],
+      ),
     );
   }
 }
