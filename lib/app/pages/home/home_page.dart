2d8a27d275d25862e52f9a463de0d4ce86fd802c
Marcelo Figueira <marcelofigueira87@gmail.com>
2023-02-01 22:49:18 -0300

    criados helpers de messages e loader

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,15 +1,34 @@
+import 'package:delivery_app/app/core/ui/helpers/loader.dart';
+import 'package:delivery_app/app/core/ui/helpers/messages.dart';
 import 'package:delivery_app/app/core/widgets/delivery_appbar.dart';
 import 'package:delivery_app/app/models/product_model.dart';
 import 'package:delivery_app/app/pages/home/widgets/delivery_product_tile.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with Loader, Messages {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: DeliveryAppbar(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          // showLoader();
+          // await Future.delayed(const Duration(seconds: 2));
+          // hideLoader();
+          showError('Erro ao clicar');
+          await Future.delayed(const Duration(seconds: 2));
+          showInfo('Erro ao clicar');
+          await Future.delayed(const Duration(seconds: 2));
+          showSucess('Erro ao clicar');
+        },
+      ),
       body: Column(
         children: [
           Expanded(
