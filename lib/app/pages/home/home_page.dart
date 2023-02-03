6af40a47a43317ebc413b623d90fef17076bc010
Marcelo Figueira <marcelofigueira87@gmail.com>
2023-02-02 23:12:30 -0300

    ajustes

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -2,8 +2,10 @@ import 'package:delivery_app/app/core/ui/helpers/loader.dart';
 import 'package:delivery_app/app/core/ui/helpers/messages.dart';
 import 'package:delivery_app/app/core/widgets/delivery_appbar.dart';
 import 'package:delivery_app/app/models/product_model.dart';
+import 'package:delivery_app/app/pages/home/home_controller.dart';
 import 'package:delivery_app/app/pages/home/widgets/delivery_product_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,6 +15,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> with Loader, Messages {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      context.read<HomeController>().loadProducts();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
